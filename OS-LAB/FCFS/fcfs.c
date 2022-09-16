2a9db9847fdc22e6c49f664e60d407f604c17113
MARRIVADA VAMSI KRISHNA REDDY <112164540+204G1A05B8@users.noreply.github.com>
2022-09-16 15:30:14 +0530

    Update fcfs.c
    create hello file

--- OS-LAB/FCFS/fcfs.c
+++ OS-LAB/FCFS/fcfs.c
@@ -2,3 +2,10 @@
 Roll no : 204g105b8
 Title of the Experiment : CPU Scheduling using FCFS Algorithm
 Date of Creation : 16-09-2022 */
+//Source code
+#include<stdio.h>
+int main()
+{
+  printf("HELLO WORLD");
+  return 0;
+}
