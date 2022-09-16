a4641057d2e2ae8d67d4c3727580e20bb63996cd
MARRIVADA VAMSI KRISHNA REDDY <112164540+204G1A05B8@users.noreply.github.com>
2022-09-16 15:18:47 +0530

    Update fcfs.c
    create fcfs file

--- os lab/exp-1/fcfs/fcfs.c
+++ os lab/exp-1/fcfs/fcfs.c
@@ -1,7 +1,7 @@
-//Roll No: 204g1a05b8//
-//Name: M.vamsi krishna reddy//
-//Exp Name: First Come First Serve//
-//Exp schedule: 16-09-2022//
+/*Roll No: 204g1a05b8
+Name: M.vamsi krishna reddy
+Exp Name: First Come First Serve
+Exp schedule: 16-09-2022*/
 #include<stdio.h>
 #include<conio.h>
 main()
