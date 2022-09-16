4acda8038599ddae30dd5afb76b20a3d60599fa5
MARRIVADA VAMSI KRISHNA REDDY <112164540+204G1A05B8@users.noreply.github.com>
2022-09-16 15:43:15 +0530

    fcfs

--- OS-LAB/FCFS/FCFS.c
+++ OS-LAB/FCFS/FCFS.c
@@ -1,9 +1,3 @@
-/*Name of the Candidate :M.vamsi krishna reddy
-Roll no : 204g105b8
-Title of the Experiment : CPU Scheduling using FCFS Algorithm
-Date of Creation : 16-09-2022 */
-//Source code
-
 #include<stdio.h>
 #include<conio.h>
 main()
